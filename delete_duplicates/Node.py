3125514242f74874abd65e41257baa0ba0db38fd
roeeash <92747574+roeeash@users.noreply.github.com>
2022-03-28 11:53:44 +0300

    delete_duplicates
    added delete duplicates with tree

--- delete_duplicates/Node.py
+++ delete_duplicates/Node.py
@@ -4,47 +4,43 @@ class Node:
         self.right = None
         self.data = data
 
-    # insert into the tree
-
     def insert(self, data):
         if self.data == data:
             return
-        elif self.data < data and self.left is None:
+        elif self.data > data and self.left is None:
             self.left = Node(data)
 
-        elif self.data > data and self.right is None:
+        elif self.data < data and self.right is None:
             self.right = Node(data)
 
         # both are not None and self.data != data
         else:
-            if self.data < data:
+            if self.data > data:
                 self.left.insert(data)
             else:
                 self.right.insert(data)
 
-    #print the tree by in-order traversal
     def print_tree(self):
-        if self.right:
-            self.right.print_tree()
-        print(self.data)
         if self.left:
             self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
 
-    # search the tree
-    
+
     def search_tree(self, data):
         if self.data == data:
             return 1
         # if smaller and no left
-        elif self.data < data and self.left is None:
+        elif self.data > data and self.left is None:
             return 0
         # if bigger and no right
-        elif self.data > data and self.right is None:
+        elif self.data < data and self.right is None:
             return 0
 
         # both are not None and self.data != data
         else:
-            if self.data < data:
+            if self.data > data:
                 self.left.search_tree(data)
             else:
                 self.right.search_tree(data)
